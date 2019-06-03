a748b511084cf45572b603e6263a5570d24e1701
Bartosz Nawrot <nawbar23@gmail.com>
2019-06-03 22:41:35 +0200

    Fix client Release after connection dropped during Idle

--- src/main/java/com/fleetmgr/sdk/client/state/device/Connected.java
+++ src/main/java/com/fleetmgr/sdk/client/state/device/Connected.java
@@ -43,7 +43,7 @@ public class Connected extends State {
     }
 
     private State onNewState(State newState) {
-        boolean dropped = internalState instanceof Recovering;
+        boolean dropped = internalState instanceof Recovering || internalState instanceof Ready;
         while (newState != null) {
             log(Level.INFO, "Connected transition: " + toString() + " -> Connected." + newState.toString());
             internalState = newState;
